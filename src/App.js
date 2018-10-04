151982349e1bb4172941c2c01c1ab6fa981afffb
mheflin259 <mheflin259@gmail.com>
2018-10-03 20:15:06 -0500

    finished assignment

--- src/App.js
+++ src/App.js
@@ -12,12 +12,34 @@ class App extends Component {
     ]
   };
 
+  handleChange = event => {
+     this.setState({ newTodo: event.target.value });
+   };
+
+   handleSubmit = event => {
+     event.preventDefault()
+     this.setState({
+       todos: [...this.state.todos, this.state.newTodo],
+       newTodo: ""
+     });
+   };
+
   render() {
     return (
       <div>
         <h1> How to make boba at home</h1>
         {this.state.todos.map(todo => (
-          <li key={todo}> {todo}</li>
+          <li key={todo}>
+               {todo + " "}
+               <form onSubmit={this.handleSubmit}>
+                  <input
+                    type="text"
+                    value={this.state.newTodo}
+                    onChange={this.handleChange}
+                  />
+                <button onClick={this.handleSubmit}>Edit</button>
+                </form>
+          </li>
         ))}
       </div>
     );
